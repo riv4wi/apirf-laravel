e65e71e62ce2ab539cf754f18f59fa6419bdebd3
riv4wi <riv4wi@gmail.com>
2018-04-15 00:51:47 -0300

    Generate 100 vehicles instead of 10

--- database/seeds/VehicleTableSeeder.php
+++ database/seeds/VehicleTableSeeder.php
@@ -11,6 +11,6 @@ class VehicleTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Vehicle::class, 10)->create();
+        factory(App\Vehicle::class, 100)->create();
     }
 }
