9391d98eb37f103c581c922ed00c3a65f0eadd2a
riv4wi <riv4wi@gmail.com>
2018-10-12 15:01:20 -0300

    AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UserController');
